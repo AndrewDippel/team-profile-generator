80b3eef614064a8ed8fe9a5b98ad5248f2a0332d
Andrew Dippel <a.t.dippel@gmail.com>
2022-10-03 12:32:21 +1100

    add manager prompts

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 const fs = require('fs');
 const path = require('path');
 const inquirer = require('inquirer');
+
+const manager = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'enter managers name',
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'employee ID?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'enter your email',
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'enter office number',
+    },
+
+]
